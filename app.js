dfe52a3e287aa239413f2f4d0aa5bf49e69e0b39
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-05-27 16:18:37 +0530

    Add array length and push example to app.js
    Add array length and push example to app.js

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ let arr = ["apple", "banana", "grapes", "melons", "mango"];
 
 console.log(arr[0]);
 console.log(arr[arr.length - 1]);
+
+
+// Finding the length of an array 
+console.log(arr.length);
+
+
+
+// adding value in array using push
+arr.push("Guava")
+
+console.log(arr);
